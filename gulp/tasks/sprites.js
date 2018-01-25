3eea377a0f377a81583a48f79882bdb4f56627ee
Quang <go.quang.nguyen@gmail.com>
2018-01-25 14:48:42 -0800

    Completed automatic sprite generation.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
-  svgSprite = require('gulp-svg-sprite');
+  svgSprite = require('gulp-svg-sprite'),
+  rename = require('gulp-rename'),
+  del = require('del');
 
 var config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -13,8 +16,35 @@ var config = {
   }
 }
 
-gulp.task('createSprite', function () {
+// Clean/delete old Sprite folders
+gulp.task('beginClean', function () {
+  return del(['./app/temp/sprite]', './app/assets/images/sprites']);
+});
+
+// Take svg images combine into one svg file, config template
+gulp.task('createSprite', ['beginClean'], function () {
   return gulp.src('./app/assets/images/icons/**/*.svg')
-    .pipe(svgSprite(config))
+    .pipe(svgSprite(config)) // Bundle all svg images into 1 file
     .pipe(gulp.dest('./app/temp/sprite'));
 });
+
+// Copy sprite graphics and organize more efficiently
+gulp.task('copySpriteGraphic', ['createSprite'], function () {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+// Copy sprite css to modules folder
+gulp.task('copySpriteCSS', ['createSprite'], function () { // [createSprite] is dependency for copySpriteCSS
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+// Delete Sprite folder in temp 
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function () {
+  return del('./app/temp/sprite');
+});
+
+// Run both createSprite & copySpriteCSS
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
