3f3d3c1981341ffbb6d4147ee0448eb1e7f0cd0c
Quang <go.quang.nguyen@gmail.com>
2018-02-08 20:32:29 -0800

    Completed sticky header behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 // Create new instances of objects
 var mobileMenu = new MobileMenu();
@@ -8,3 +9,6 @@ var mobileMenu = new MobileMenu();
 // Reusing RevealOnScroll object for diff elements
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
+
